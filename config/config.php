9104c97189e2eed532dd8e6ebe71941fa8c8682c
Bader Binsunbil <baziz22@gmail.com>
2022-08-24 05:19:01 +0300

    Updating config file

--- config/config.php
+++ config/config.php
@@ -4,24 +4,24 @@
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'b');
-define('DB_PASS', '');
+define('DB_PASS', 'b');
 define('DB_NAME', 'mvc');
 
 // APPROOT
 define('APPROOT', dirname(dirname(__FILE__)));
-
+//echo "ApprUUUUUoot: ", APPROOT;
 // URLROOT (Dynamic Links)
-define('URLROOT', './');
-
+define('URLROOT', '');
 // SITENAME
 define('SITENAME', 'Bader Binsunbil');
 
-const BR = "<br />";
-function pre_r(array $array)
+include_once URLROOT . 'views/includes/shortcut.php';
+//const BR = "<br />";
+/* function pre_r(array $array)
 {
     echo '<pre>';
     print_r($array);
     echo '</pre>';
-}
+} */
 
 //define('DB', $this->db->connect());
